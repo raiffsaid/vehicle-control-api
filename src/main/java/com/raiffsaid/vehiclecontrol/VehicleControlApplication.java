47a13e3d3fe8e2dbba6220d68a28c9970281b38c
Raiff Smith Said <raiff.said@gmail.com>
2021-06-11 12:35:17 -0300

    Project created

---
+++ src/main/java/com/raiffsaid/vehiclecontrol/VehicleControlApplication.java
@@ -0,0 +1,13 @@
+package com.raiffsaid.vehiclecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raiffsaid/vehiclecontrol/VehicleControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raiffsaid.vehiclecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
