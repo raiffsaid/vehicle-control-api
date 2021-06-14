791685111be82fc893ad313f6dd0c6d23371a1b0
Raiff Smith Said <raiff.said@gmail.com>
2021-06-14 01:02:02 -0300

    fix: Adjusted OpenFeign annotation on app entrypoint

--- src/main/java/com/raiffsaid/vehiclecontrol/VehicleControlApplication.java
+++ src/main/java/com/raiffsaid/vehiclecontrol/VehicleControlApplication.java
@@ -2,10 +2,10 @@ package com.raiffsaid.vehiclecontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@FeignClient
+@EnableFeignClients
 public class VehicleControlApplication {
 
     public static void main(String[] args) {
